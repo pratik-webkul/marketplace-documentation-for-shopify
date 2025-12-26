3e4d9e28a4f40d765d54b2d05adf4e0fe2655d0c
pratik <pratik@webkul.com>
2025-12-26 12:27:37 +0530

    change inworkflow

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base : '/',
   lang: 'en',
   title: 'Multivendor Marketplace For Shopify',
   description: 'This is the documentation regading webkul multivendor marketplace for shopify',
